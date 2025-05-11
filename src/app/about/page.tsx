a60e346f6ee8fa53952631a4085ca6f307985fcb
Chloriiin <yeasterucha@gmail.com>
2025-05-11 14:44:06 -0400

    Adjust About page: section spacing, subtitle line height, image size, and modernize Next/Image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -74,7 +74,7 @@ export default function About() {
           <hr className="w-full border-gray-300 my-2" />
           
           <div className="w-full flex flex-col md:flex-row gap-8 mt-1 px-2 sm:px-0">
-            <div className="md:w-2/3 flex flex-col gap-6">
+            <div className="md:w-2/3 flex flex-col">
               <div className="flex flex-col sm:flex-row">
                 <div className="flex-grow sm:pr-6">
                   <p className="text-lg text-black mb-2">
@@ -89,7 +89,7 @@ export default function About() {
                 </div>
               </div>
 
-              <div className="flex flex-col sm:flex-row">
+              <div className="flex flex-col sm:flex-row mt-[150px]">
                 <div className="flex-grow sm:pr-6">
                   <p className="text-lg text-black">
                     While working at Sarafianos Lab, I am particularly fascinated by how numerical methods as well as mathematical modeling can be integrated with experimental biology to elucidate complex biochemical mechanisms. My work includes developing and optimizing curve-fitting algorithms to improve the accuracy of Thermal Shift Assay data interpretation.
@@ -97,19 +97,20 @@ export default function About() {
                 </div>
                 <div className="flex-shrink-0 flex flex-col sm:items-start justify-start mt-2 sm:mt-0">
                   <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap">Applied</h2>
-                  <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap -mt-2">Math</h2>
+                  <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap mt-1">Math</h2>
                 </div>
               </div>
             </div>
 
             <div className="md:w-1/3 mt-4 md:mt-0">
-              <div className="relative w-full h-auto aspect-[3/4]">
+              <div className="relative w-4/5 mx-auto aspect-[3/4]">
                 <Image 
                   src="/imgs/profile_trimmed.jpg"
                   alt="Zhijiang (Zach) Ye profile picture"
-                  layout="fill"
-                  objectFit="cover"
+                  fill
+                  style={{ objectFit: 'cover' }}
                   className="rounded-lg"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 />
               </div>
             </div>
