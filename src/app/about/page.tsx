5a5107f65100044998981dfa2201daaee3e814ea
Chloriiin <yeasterucha@gmail.com>
2025-05-11 15:08:23 -0400

    Restructure About page to three-column grid layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -73,37 +73,36 @@ export default function About() {
 
           <hr className="w-full border-gray-300 my-2" />
           
-          <div className="w-full flex flex-col md:flex-row gap-8 mt-1 px-2 sm:px-0">
-            <div className="md:w-2/3 flex flex-col gap-6">
-              <div className="flex flex-col sm:flex-row sm:items-start">
-                <div className="flex-grow sm:pr-6">
-                  <p className="text-lg text-black mb-2">
-                    Currently I am working as an undergraduate researcher in the Sarafianos Lab at Emory Pediatrics. Here I investigate interaction dynamics between engineered HIV-1 Capsid protein (CA121) mutants and specific inhibitory compounds.
-                  </p>
-                  <p className="text-lg text-black">
-                    My academic interests are focused particularly in biochemical pharmacology and structural biology.
-                  </p>
-                </div>
-                <div className="flex-shrink-0 flex items-start justify-start pt-[150px] sm:pt-[150px]">
-                  <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap">biology</h2>
-                </div>
+          <div className="w-full grid grid-cols-1 md:grid-cols-[2fr_1.5fr_1fr] gap-x-8 mt-1 px-2 sm:px-0 md:items-start">
+            
+            <div className="flex flex-col">
+              <div>
+                <p className="text-lg text-black mb-2">
+                  Currently I am working as an undergraduate researcher in the Sarafianos Lab at Emory Pediatrics. Here I investigate interaction dynamics between engineered HIV-1 Capsid protein (CA121) mutants and specific inhibitory compounds.
+                </p>
+                <p className="text-lg text-black">
+                  My academic interests are focused particularly in biochemical pharmacology and structural biology.
+                </p>
               </div>
-
-              <div className="flex flex-col sm:flex-row sm:items-start">
-                <div className="flex-grow sm:pr-6">
-                  <p className="text-lg text-black">
-                    While working at Sarafianos Lab, I am particularly fascinated by how numerical methods as well as mathematical modeling can be integrated with experimental biology to elucidate complex biochemical mechanisms. My work includes developing and optimizing curve-fitting algorithms to improve the accuracy of Thermal Shift Assay data interpretation.
-                  </p>
-                </div>
-                <div className="flex-shrink-0 flex flex-col items-start justify-start pt-[150px] sm:pt-[150px]">
-                  <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap">Applied</h2>
-                  <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap mt-1">Math</h2>
-                </div>
+              <div className="mt-10">
+                <p className="text-lg text-black">
+                  While working at Sarafianos Lab, I am particularly fascinated by how numerical methods as well as mathematical modeling can be integrated with experimental biology to elucidate complex biochemical mechanisms. My work includes developing and optimizing curve-fitting algorithms to improve the accuracy of Thermal Shift Assay data interpretation.
+                </p>
               </div>
             </div>
 
-            <div className="md:w-1/3 mt-4 md:mt-0">
-              <div className="relative w-4/5 mx-auto h-auto aspect-[3/4]">
+            <div className="flex flex-col md:pt-0">
+              <div className="mt-4 md:mt-0">
+                <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap">biology</h2>
+              </div>
+              <div className="mt-[150px]">
+                <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap">Applied</h2>
+                <h2 className="text-5xl font-bold text-[#86CECB] whitespace-nowrap mt-1">Math</h2>
+              </div>
+            </div>
+
+            <div className="mt-8 md:mt-0">
+              <div className="relative w-full sm:w-4/5 mx-auto h-auto aspect-[3/4]">
                 <Image 
                   src="/imgs/profile_trimmed.jpg"
                   alt="Zhijiang (Zach) Ye profile picture"
